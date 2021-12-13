0b0cd4cf4a8920781ad49e2e68c0caafc0892a9d
Kileab <82032086+Unity2033@users.noreply.github.com>
2021-12-13 22:42:34 +0900

    Life cycle explanation
    - Unity event function description
    
    - Call order and function test for
      each function.
    
    - Conveyor belt movement.

---
+++ Assets/Script/Conveyor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conveyor : MonoBehaviour
+{
+    public GameObject Belt;
+    public Transform Point;
+    public float speed;
+
+    private void OnTriggerStay(Collider other)
+    {
+        other.transform.position = Vector3.MoveTowards
+           (
+            other.transform.position,
+            Point.position, 
+            speed * Time.deltaTime
+           );
+    }
+}
