55e620550b91a9c1e6baad71f30bf78db58923fe
Kileab <82032086+Unity2033@users.noreply.github.com>
2021-12-26 18:59:18 +0900

    Added scene movement system
    - Use the input field to set the scene
      to switch to the input number.
    
    - Add and delete resource data.
    
    - Skybox 360 degree rotation system
      setting.

---
+++ Assets/Script/Scene_Select.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Scene_Select : MonoBehaviour
+{
+    public InputField input; 
+    
+    public void Scene_Number()
+    {
+       SceneManager.LoadScene(int.Parse(input.text));   
+    }
+}
