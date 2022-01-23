8e9fa7ba357e1180b289fa501298732f0ee1bd9f
Kileab <82032086+Unity2033@users.noreply.github.com>
2022-01-23 23:05:33 +0900

    Animation system design.
    - It's designed to create animations
      naturally through a blend tree.
    
    - Design a system in which animation
      operates according to specific
      conditions.
    
    - Designing animation connection
      work with animation transition.
    
    - Add and delete resource data.

---
+++ Assets/Script/Animation Training/Animation_Manager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation_Manager : MonoBehaviour
+{
+    public Animator animator;
+
+    float horizontal;
+    float vertical;
+
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal"); // 수평의 정보를 나타냅니다.  
+        vertical = Input.GetAxis("Vertical");     // 수직의 정보를 나타냅니다.
+
+        // 키보드 입력에 따라 -1 ~ 1 까지의 정보가 저장됩니다.
+
+        animator.SetFloat("horizontal", horizontal);
+        animator.SetFloat("vertical", vertical);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.Play("Jump", -1, 0); 
+        }
+    }
+}
--- Assets/Script/Roll Ball/Ball_Control.cs
+++ Assets/Script/Roll Ball/Ball_Control.cs
@@ -21,16 +21,9 @@ public class Ball_Control : MonoBehaviour
 
     void FixedUpdate()
     {
-        //if(Input.GetButtonDown("Jump"))
-        //{
-        //    rigid.AddForce(Vector3.up * 50, ForceMode.Impulse);
-        //}
-
         Vector3 vec = new Vector3
         (
-              Input.GetAxisRaw("Horizontal"),
-              0,
-              Input.GetAxisRaw("Vertical")
+              Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")
          );
 
         rigid.AddForce(vec, ForceMode.Impulse);
