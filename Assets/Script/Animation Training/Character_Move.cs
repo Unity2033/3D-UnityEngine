ff1548d83b1f82830f73febb94fa8427e83f20d0
Kileab <82032086+Unity2033@users.noreply.github.com>
2022-02-06 23:19:23 +0900

    Save and import data from outside of Json data
    - Increase the character Y axis and
      set the inability to move when
      jumping.
    
    - Establish an external path so that it
      can be stored and retrieved from
      the outside.
    
    - Delete and add resource data

--- Assets/Script/Animation Training/Character_Move.cs
+++ Assets/Script/Animation Training/Character_Move.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Character_Move : MonoBehaviour
 {
+    [SerializeField] Animator animator;
     [SerializeField] float speed = 1.0f; // 이동속도
 
     float gravity = 9.81f; // 중력 계수
@@ -23,6 +24,25 @@ public class Character_Move : MonoBehaviour
         {
             // y축에 중력의 계수가 적용되도록 설정합니다.
             Direction.y -= gravity * Time.deltaTime;
+        }
+        else
+        {
+            float vertical = Input.GetAxis("Vertical"); // 수직의 정보를 나타냅니다.
+                                                        // 키보드 입력에 따라 -1 ~ 1 까지의 정보가 저장됩니다.     
+
+            float horizontal = Input.GetAxis("Horizontal");
+
+            // 애니메이션의 SetFloat()함수는 단발성 호출에 사용되기 때문에 Get 함수가 없습니다.
+            animator.SetFloat("vertical", vertical);
+            animator.SetFloat("horizontal", horizontal);
+
+            Move_To(new Vector3(horizontal, 0, vertical));
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.Play("Jump", -1, 0);
+            Direction = new Vector3(0, 5f, 0);
         }
 
         character.Move(Direction * speed * Time.deltaTime);
--- Assets/Script/Initial Screen/Json_Data_Manager.cs
+++ Assets/Script/Initial Screen/Json_Data_Manager.cs
@@ -1,11 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
-// 1. 데이터(코드 = 클래스)를 만들어야 합니다. -> 저장할 데이터를 생성해야합니다.
-// 2. 그 데이터를 Json으로 변환합니다.
+// 데이터를 저장하는 방법
+// 1. 저장할 데이터가 존재해야 합니다.
+// 2. 데이터를 Json으로 변환합니다.
+// 3. Json을 외부에 저장합니다.
 
-class Data
+// 불러오는 방법
+// 1. 외부에 저장된 Json을 가져옵니다.
+// 2. Json을 데이터형태로 변환합니다.
+// 3. 불러올 데이터를 사용합니다.
+
+
+public class Data // 저장을 하기 위한 데이터
 {
     public string name;
     public int age;
@@ -14,20 +23,47 @@ class Data
 
 public class Json_Data_Manager : MonoBehaviour
 {
+    public static Json_Data_Manager instance;
+
+    string path;
+    string filename = "save";
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else if(instance != this)
+        {
+            Destroy(instance.gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        path = Application.persistentDataPath + "/";
+    }
+
     Data player = new Data() { name = "사람", age = 10, money = 20 };
 
-    void Start()
+    public void Save_Data()
     {
-        // 2. Json으로 변환
+        // < Json으로 변환 >
         // Json은 string이기 때문에 string 변수에 넣어주어야 합니다.
-       string json_Data = JsonUtility.ToJson(player);
+        string json_Data = JsonUtility.ToJson(player);
 
-       // Json을 다시 코드로 바꾸는 방법
-       Data player_ = JsonUtility.FromJson<Data>(json_Data);
+        print(path);
+        File.WriteAllText(path + filename, json_Data);
     }
 
-    void Update()
+    public void Load_Data()
     {
-        
+        string data = File.ReadAllText(path + filename);
+
+        // data가 <Data> 형식으로 변환됩니다.
+        // 불러온 데이터 player 덮어 쓰입니다.
+        player = JsonUtility.FromJson<Data>(data);
     }
+
+
 }
