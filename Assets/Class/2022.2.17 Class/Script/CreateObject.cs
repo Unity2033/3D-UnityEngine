2158c18c026d7fa8932b958130cab8d165744e6b
Kileab <82032086+Unity2033@users.noreply.github.com>
2022-02-17 11:17:05 +0900

    Attach and add data.
    - Attach and add PPT data.
    
    - Add and delete resource data.

---
+++ Assets/Class/2022.2.17 Class/Script/CreateObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateObject : MonoBehaviour
+{
+    public GameObject cube;
+    GameObject obj;
+
+    void Start()
+    {   
+        Instantiate(cube, new Vector3(0,1,0), Quaternion.identity);
+        obj = GameObject.Find("Cube(Clone)");
+    }
+
+    void Update()
+    {
+        if(Input.GetKey("a"))
+        {
+            Destroy(obj);
+        }
+    }
+}
