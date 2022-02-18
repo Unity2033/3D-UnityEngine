1af5bd5e3c55d7b2813a51639bfa0b9dde9d2a1d
Kileab <82032086+Unity2033@users.noreply.github.com>
2022-02-18 11:21:56 +0900

    Implement object pooling
    - Designing object pooling using data
      structure queues
    
    - Add lecture materials.

---
+++ Assets/Class/2022.2.18 Class/Script/ObjectPool.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    public static ObjectPool objpool;
+
+    public GameObject obj;
+
+    public Queue<GameObject> mqueue = new Queue<GameObject>();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objpool = this;
+
+        for(int i = 0; i < 10; i++)
+        {
+            GameObject tobj = Instantiate(obj, Vector3.zero, Quaternion.identity);
+            mqueue.Enqueue(tobj);
+            tobj.gameObject.SetActive(false);
+        }
+    }
+
+    public void InsertQueue(GameObject pobj)
+    {
+        mqueue.Enqueue(pobj);
+        pobj.gameObject.SetActive(false);
+    }
+
+    public GameObject GetQueue()
+    {
+        GameObject tobj =  mqueue.Dequeue();
+        tobj.gameObject.SetActive(true);
+
+        return tobj;
+    }
+  
+}
