0cfbfeeb1639d874fc654fd2c98562f0ba747ad9
Kileab <82032086+Unity2033@users.noreply.github.com>
2022-02-23 11:05:32 +0900

    Coroutine explanation and practice
    - Setting up a delay system using
      coroutines
    
    - Implement automatic movement
      with spherical linear interpolation
    
    - Set an object to look at itself
      through another look-at function

---
+++ Assets/Class/2022.2.23/Script/Control.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+    bool delay;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime; 
+        float z = Input.GetAxis("Vertical") * Time.deltaTime;
+
+        transform.Translate(x, 0, z);
+
+        int layerMask = 1 << LayerMask.NameToLayer("Monster");
+
+        var ray = new Ray(this.transform.position, this.transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100, layerMask))
+        {
+            Debug.Log("냥");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(!delay)
+            {
+                delay = true;
+                Debug.Log("공격");
+                StartCoroutine(AttackDelay());
+            }
+            else
+            {
+                Debug.Log("딜레이");
+            }
+        }
+    }
+
+    IEnumerator AttackDelay()
+    {
+        yield return new WaitForSeconds(2f);
+        delay = false;
+    }
+}
---
+++ Assets/Class/2022.2.23/Script/Parabola.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Parabola : MonoBehaviour
+{
+    // 최종적으로 이동할 기준점이 되는 게임 오브젝트
+    public GameObject target;
+
+    // 이동하는 속도 변수
+    float speed = 1.0f;
+
+    int value = 0;
+
+    private void Start()
+    {
+        InvokeRepeating("Increase", 0, 2);
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.Slerp(transform.position, target.transform.position, speed * Time.deltaTime);
+
+        if(value % 2 == 0)
+        {
+            target.transform.position = new Vector3(4, 2, 3);
+        }
+        else
+        {
+            target.transform.position = new Vector3(-4, 2, 3);
+        }
+
+    }
+
+    void Increase()
+    {
+        value++;
+    }
+}
