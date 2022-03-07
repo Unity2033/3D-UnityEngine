b19ea7c985bcaaada071fde968c335eaf97135b4
Kileab <82032086+Unity2033@users.noreply.github.com>
2022-03-07 11:26:04 +0900

    Added animator system
    - Has Exit Time Description and
      Practice
    
    - Add animation clips and set the
      timeline
    
    - Link transitions and set animation
      behavior

---
+++ Assets/Class/2022.3.7/Script/Controller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            // 실행할 게임 스테이트의 이름을 설정합니다.
+            animator.Play("Run");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {     
+            animator.SetBool("Jump",true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Return))
+        {
+            animator.SetBool("Jump", false);
+        }
+    }
+}
