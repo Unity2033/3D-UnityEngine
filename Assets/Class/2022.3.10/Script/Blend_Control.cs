f953b04bb6d65aee53105b330f33f9ef6bf4a85a
Kileab <82032086+Unity2033@users.noreply.github.com>
2022-03-10 11:11:35 +0900

    How to use Blend Tree
    - The camera enlargement and
      reduction range were set using the
      clamp function.
    
    - Move and interact using character
      collider.
    
    - Connect animation using Blend
      Tree.
    
    - Mecca's animation explanation and
      practice.

---
+++ Assets/Class/2022.3.10/Script/Blend_Control.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blend_Control : MonoBehaviour
+{
+    Animator animator;
+    CharacterController control;
+
+    public float speed = 1.0f;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        control = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        animator.SetFloat("Horizontal", h);
+        animator.SetFloat("Vertical", v);
+
+        Vector3 dir = new Vector3(h, 0, v);
+
+        control.Move(dir.normalized * speed * Time.deltaTime);
+    }
+}
