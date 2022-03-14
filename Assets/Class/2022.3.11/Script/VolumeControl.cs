4a9465508831bc5540761347280054fa4b434126
Kileab <82032086+Unity2033@users.noreply.github.com>
2022-03-14 11:10:14 +0900

    Added volume control system
    - Set the volume control of the audio
      source using the slider
    
    - Set up animation layers and create
      avatars

---
+++ Assets/Class/2022.3.11/Script/VolumeControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeControl : MonoBehaviour
+{
+    public AudioSource audioa;
+
+    public void Set_Volume(float v)
+    {
+        audioa.volume = v;
+    }
+}
