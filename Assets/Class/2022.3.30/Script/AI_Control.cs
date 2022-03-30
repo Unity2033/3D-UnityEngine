8dc0039111bfab876aa00412f0485df1a6ebe99e
Kileab <82032086+Unity2033@users.noreply.github.com>
2022-03-30 11:32:50 +0900

    NavMesh Obstacle Add
    - Implementation of agent
      movement system through mouse
      input
    
    - Moving structures away from each
      other using an open link mesh
    
    - Add and delete resource data

---
+++ Assets/Class/2022.3.30/Script/AI_Control.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI_Control : MonoBehaviour
+{
+    NavMeshAgent agent;
+
+    public Transform arrive;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        agent.SetDestination(arrive.position);
+    }
+}
