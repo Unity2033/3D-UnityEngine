b3bd2d428909fc07f7d76525a990c8ac7a258a9d
Kileab <82032086+Unity2033@users.noreply.github.com>
2022-04-11 01:55:16 +0900

    How to use each joint
    - Introduction of hinge function
      using hinge joint
    
    - Design of Game Object Connection
      System Using Fixed Joints
    
    - Lighting usage and optimization

---
+++ Assets/Class/2022.4.11/Script/Sound.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Sound : MonoBehaviour
+{
+    AudioSource source;
+    public AudioClip[] album;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            source.clip = album[0];
+            source.Play();
+        }
+        else if(Input.GetKeyDown(KeyCode.F2))
+        {
+            source.clip = album[1];
+            source.Play();
+        }
+        else if (Input.GetKeyDown(KeyCode.F3))
+        {
+            source.clip = album[2];
+            source.Play();
+        }
+        else if (Input.GetKeyDown(KeyCode.F4))
+        {
+            source.clip = album[3];
+            source.Play();
+        }
+        else if (Input.GetKeyDown(KeyCode.F5))
+        {
+            SceneManager.LoadScene("2022.4.11.new");
+        }
+
+
+    }
+}
