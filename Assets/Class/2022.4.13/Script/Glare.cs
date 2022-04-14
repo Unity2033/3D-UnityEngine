240b34cacc787a41b0bcdc2646978dee0624cab0
Kileab <82032086+Unity2033@users.noreply.github.com>
2022-04-15 02:12:57 +0900

    How to use the Terrain System
    - Using and setting up the particle
      system
    
    - Creating maps with the Terrain
      System
    
    - Realize flickering effect using light
      intensity

---
+++ Assets/Class/2022.4.13/Script/Glare.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Glare : MonoBehaviour
+{
+    Light glare;
+ 
+
+    void Start()
+    {
+        glare = GetComponent<Light>();
+    }
+
+    void Update()
+    {
+           glare.intensity = Random.Range(0.2f, 0.8f);     
+    }
+}
