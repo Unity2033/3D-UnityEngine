15d71b5dd83bc6746448f8e2350ba02cd06f9237
Kileab <82032086+Unity2033@users.noreply.github.com>
2022-04-20 02:36:33 +0900

    Game Design
    - Implementation of navigation using
      render textures
    
    - Save the location in an array format
      and set it to be moved

---
+++ Assets/Class/Portfolio/Script/Character_Controller.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Character_Controller : MonoBehaviour
+{
+    public float speed = 5.0f;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical"); 
+
+        Vector3 dir = new Vector3(x, 0, z);
+
+        transform.Translate(dir.normalized * speed * Time.deltaTime );
+
+        transform.Rotate(0f, Input.GetAxis("Mouse X") * speed, 0f, Space.World);
+    }
+}
---
+++ Assets/Class/Portfolio/Script/Monster_Control.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Monster_Control : MonoBehaviour
+{
+    NavMeshAgent agent;
+
+    public Transform[] Point;
+    int count = 0;
+
+    void Patrol()
+    {
+        if(agent.velocity == Vector3.zero)
+        {
+            agent.SetDestination(Point[count++].position);
+
+            if(count >= Point.Length)
+            {
+                count = 0;
+            }
+        }
+    }
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        InvokeRepeating("Patrol", 0, 1f);
+    }
+
+    void Update()
+    {
+        
+    }
+}
