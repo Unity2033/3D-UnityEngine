e947f85ee0388821243b66a2b845f7eff6cb2f67
Kileab <82032086+Unity2033@users.noreply.github.com>
2022-06-26 21:45:01 +0900

    Particle Tutorial Renewal
    - Implemented to move particles in a
      parabolic shape using a spherical
      linear interpolation function
    
    - Add and delete resource data

---
+++ Assets/Class/Particle/Script/ParticleManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleManager : MonoBehaviour
+{
+    ParticleSystem particle;
+
+    private void Start()
+    {
+        particle = GetComponent<ParticleSystem>();
+    }
+
+    public void Selected()
+    {
+        particle.Play();
+    }
+}
