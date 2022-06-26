f371c8fe947f6f4df6807b2229d77deb01a0bdfe
Kileab <82032086+Unity2033@users.noreply.github.com>
2022-06-27 05:11:17 +0900

    Rotation Tutorial Renewal
    - Add and delete resource data
    
    - Measure the distance between two
      objects so that the object can look
      at them

---
+++ Assets/Class/Rotation/Script/Bicycle.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bicycle : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Rotate(0.1f, 0.1f, 0.1f);
+    }
+}
---
+++ Assets/Class/Rotation/Script/Observe.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Observe : MonoBehaviour
+{
+    // 바라보기 위한 게임 오브젝트
+    public GameObject target;
+
+    void Update()
+    {
+        // 자기 위치와 target의 위치가 5보다 작으면 대상을 바라보도록 설정합니다.
+        if(Vector3.Distance(transform.position, target.transform.position) <= 5)
+        {
+            transform.LookAt(target.transform);
+        }
+        else // 그렇지 않다면 회전 값은 15, 90,0 처음 초기값으로 돌려놓습니다.
+        {
+            transform.rotation = Quaternion.Euler(15, 90, 0);
+        }
+    }
+}
