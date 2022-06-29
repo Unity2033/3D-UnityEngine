bb68b80fbe774f4616b32bf1d88fa4159cd1cd7f
Kileab <82032086+Unity2033@users.noreply.github.com>
2022-06-30 01:29:28 +0900

    Light Tutorial Renewal
    - Light Probe Placement
    
    - Enabling and disabling light objects
      using buttons

---
+++ Assets/Class/Light/Script/LightManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class LightManager : MonoBehaviour
+{
+    bool condition = false;
+    public GameObject [] lightEffect;
+
+    public void LightSetting(int number)
+    {   
+        condition = !condition;
+
+        if (condition)
+        {
+            lightEffect[number].SetActive(true);
+        }
+        else
+        {
+            lightEffect[number].SetActive(false);
+        }
+    }
+}
