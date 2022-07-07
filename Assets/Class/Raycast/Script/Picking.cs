377d212adc96c7131bb84801f120b1f960acce28
Kileab <82032086+Unity2033@users.noreply.github.com>
2022-07-07 20:05:24 +0900

    Added raycast mask tutorial
    - Loading objects from resource folder
      when collided with raycast

--- Assets/Class/Raycast/Script/Picking.cs
+++ Assets/Class/Raycast/Script/Picking.cs
@@ -2,6 +2,8 @@
 
 public class Picking : MonoBehaviour
 {
+    public LayerMask layer;
+
     void Update()
     {
         // GetMouseButton(0) : 마우스 왼쪽 버튼 클릭을 했을 때     
@@ -11,11 +13,15 @@ public class Picking : MonoBehaviour
 
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-            // 광선을 발사한 다음 게임 오브젝트가 있다면 hit 변수에 데이터를 저장합니다. 
-            if (Physics.Raycast(ray, out hit))
+            // 광선을 발사한 다음 광선과 충돌한 게임 오브젝트가 있다면 hit 변수에 데이터를 저장합니다. 
+            // layer는 해당 layer만 검출하여 충돌을 처리합니다.
+            if (Physics.Raycast(ray, out hit, layer))
             {
-                Debug.DrawLine(ray.origin, hit.point, Color.red);
+                  hit.collider.gameObject.GetComponent<Renderer>().material = Resources.Load<Material>("Ice");                        
             }
         }
     }
 }
+
+
+
