6c85b391d2438c9ea2b6a2ed21e962b8537699c8
Kileab <82032086+Unity2033@users.noreply.github.com>
2022-07-10 23:44:38 +0900

    Import text file
    - I have specified a path and set it to
      load the contents of a text file.
    
    - How to set up animation buttons

---
+++ Assets/Class/Animation Clip/Script/FileLoad.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class FileLoad : MonoBehaviour
+{
+    public Text textUI;
+
+    string path = "Assets/Class/Animation Clip/File/Explanation.txt";
+
+    void Start()
+    {
+        textUI.text = System.IO.File.ReadAllText(path);
+    }
+
+}
