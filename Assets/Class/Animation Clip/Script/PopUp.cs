e7df42c1987a8d2477b4cf4b3abab71ad955b1ae
Kileab <82032086+Unity2033@users.noreply.github.com>
2022-07-22 14:51:20 +0900

    Animator Manual Renewal
    - Change the way the animator
      component is imported

--- Assets/Class/Animation Clip/Script/PopUp.cs
+++ Assets/Class/Animation Clip/Script/PopUp.cs
@@ -1,24 +1,19 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PopUp : MonoBehaviour
 {
-    Animator anim;
+    public Animator animator;
     public GameObject window;
-
-    void Start()
-    {
-        anim = window.GetComponent<Animator>();
-    }
 
     void Update()
     {
         // 애니메이터 컨트롤러에서 현재 애니메이터의 상태의 이름이“close”일 때 
-        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Close"))
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Close"))
         {
-            // 현재 애니메이션의 진행도가 1보다 크거나 같다면 User Interface를 비활성화하도록 설계하였습니다.
-            if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)
+            // 현재 애니메이션의 진행도가 1보다 크거나 같다면 User Interface를 비활성화합니다.
+            if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)
             {
                 window.SetActive(false);
             }
@@ -27,7 +22,7 @@ public class PopUp : MonoBehaviour
 
     public void Close()
     {
-        anim.SetTrigger("Close");
+        animator.SetTrigger("Close");
     }
 
     public void PopUpOpen()
