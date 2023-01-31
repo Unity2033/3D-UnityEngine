064949d9421505e94daec5faa2361400d23866ea
Kileab <82032086+Unity2033@users.noreply.github.com>
2023-01-31 14:56:52 +0900

    Collision Tutorial Renewal
    - Implemented so that material
      information changes when a
      collision occurs without physical
      collision
    
    - Collision PPT Data update
    
    - Delete and add resource data

--- Assets/Class/Collision/Script/Sensor.cs
+++ Assets/Class/Collision/Script/Sensor.cs
@@ -3,34 +3,24 @@ using UnityEngine.Video;
 
 public class Sensor : MonoBehaviour
 {
-    public Mesh [] mesh;
-    private VideoPlayer video;
-    private MeshFilter meshFilter;
-
-    private void Start()
-    {
-        meshFilter = GetComponent<MeshFilter>();
-        video = GetComponent<VideoPlayer>();
-    }
-
+    public Material[] material;
+ 
     // 물리적인 충돌을 하지 않고 충돌을 했을 때 동작하는 함수입니다.
     private void OnTriggerEnter(Collider other)
     {
-        video.Play();
-        video.isLooping = true;
+        gameObject.GetComponent<MeshRenderer>().material = material[0];
     }
 
     // 물리적인 충돌을 하지 않고 충돌을 하고 있을 때 동작하는 함수입니다.
     private void OnTriggerStay(Collider other)
     {
-        meshFilter.mesh = mesh[0];
+   
     }
 
     // 물리적인 충돌을 하지 않고 충돌을 벗어났을 때 동작하는 함수입니다.
     private void OnTriggerExit(Collider other)
     {
-        video.Stop();
-        meshFilter.mesh = mesh[1];
+        gameObject.GetComponent<MeshRenderer>().material = material[1];
     }
 
 }
