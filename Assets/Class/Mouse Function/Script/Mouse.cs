d468bfd14f7a9b6433d1f9d0b4d8e991a0bd640e
Kileab <82032086+Unity2033@users.noreply.github.com>
2023-02-08 16:38:13 +0900

    Define how to use Raycast
    - Raycast PPT Data Update
    
    - Implementation of On/Off function
      of video player using Raycast
    
    - Add and set up layers

--- Assets/Class/Mouse Function/Script/Mouse.cs
+++ Assets/Class/Mouse Function/Script/Mouse.cs
@@ -3,9 +3,31 @@ using UnityEngine.Video;
 
 public class Mouse : MonoBehaviour
 {
+    bool power = false;
+    private RaycastHit hit;
+
     [SerializeField] Rigidbody rigid;
     [SerializeField] GameObject state;
     [SerializeField] VideoPlayer video;
+    [SerializeField] LayerMask layerMask;
+
+    private void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
+            {
+                power = !power;
+
+                if (power == true)
+                    video.Play();
+                else
+                    video.Stop();
+            }
+        }
+    }
 
     private void OnMouseDown()
     {
@@ -16,7 +38,7 @@ public class Mouse : MonoBehaviour
     private void OnMouseDrag()
     {
         // 마우스의 위치를 설정합니다.
-        Vector3 mouseposition = new Vector3
+        Vector3 mousePosition = new Vector3
         (
              Input.mousePosition.x, 
              Input.mousePosition.y, 
@@ -24,9 +46,9 @@ public class Mouse : MonoBehaviour
         );
 
         //마우스 좌표를 ScreenToWorldPoint로 변경하여 오브젝트의 위치로 변경합니다.
-        Vector3 objposition = Camera.main.ScreenToWorldPoint(mouseposition);
+        Vector3 objectPosition = Camera.main.ScreenToWorldPoint(mousePosition);
 
-        transform.position = objposition;
+        transform.position = objectPosition;
     }
 
     private void OnMouseUp()
