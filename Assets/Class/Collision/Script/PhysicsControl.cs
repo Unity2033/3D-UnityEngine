d61b513103f67f516528ad4325c1ea03ae501297
Kileab <82032086+Unity2033@users.noreply.github.com>
2023-02-27 23:17:38 +0900

    Collision Tutorial Renewal
    - Light PPT Data Update
    
    - Adding and deleting resource data
    
    - Tutorial plans for physical content

---
+++ Assets/Class/Collision/Script/PhysicsControl.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsControl : MonoBehaviour
+{
+    public float speed;
+
+    Rigidbody rigid;
+    Vector3 direction;
+    
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        direction.x = Input.GetAxis("Horizontal");
+        direction.z = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+         rigid.MovePosition
+         (
+              rigid.position + direction.normalized *
+              speed * Time.deltaTime
+         );
+    }
+
+    // 물리적인 충돌을 했을 때 동작하는 함수입니다.
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Fence"))
+        {
+         
+        }
+    }
+
+    // 물리적인 충돌을 하고 있을 때 동작하는 함수입니다.
+    private void OnCollisionStay(Collision collision)
+    {
+       
+    }
+
+    // 물리적인 충돌을 벗어났을 때 동작하는 함수입니다.
+    private void OnCollisionExit(Collision collision)
+    {
+      
+        if (collision.gameObject.CompareTag("Fence"))
+        {
+            
+        }
+    }
+}
